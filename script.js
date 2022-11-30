49c88bd81f308c866670c00d8e24953c2a74828c
MacheteBy <Macheteby@gmail.com>
2022-11-30 11:24:53 +0300

    first

--- script.js
+++ script.js
@@ -174,3 +174,20 @@ class Kata {
       return length * width * height;
     }
   }
+
+
+
+
+//next
+
+function sumStr(a,b) {
+  return String(Number(a) + Number(b));
+}
+
+
+console.log(sumStr("4","5"));
+
+
+//next
+
+
