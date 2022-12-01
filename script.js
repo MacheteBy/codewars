f7ed78521b3bc86d1fcb7f7e0bca21fa2c53e1d3
MacheteBy <Macheteby@gmail.com>
2022-12-01 13:02:42 +0300

    first

--- script.js
+++ script.js
@@ -303,3 +303,12 @@ function addBinary(a,b) {
 }
 
 console.log(addBinary(5, 4));
+
+//next
+
+function cockroachSpeed(s) {
+    return Math.floor(s * 100000 / 3600);
+}
+
+
+  console.log(cockroachSpeed(1.08));
