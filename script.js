aa4a97709592b78c131d0a87c37653881ad64c5d
MacheteBy <Macheteby@gmail.com>
2022-12-03 11:04:28 +0300

    first

--- script.js
+++ script.js
@@ -432,4 +432,9 @@ function findShort(s) {
     return Math.min(...arrNum);
 }
 
+
+function findShort(s){
+    return Math.min(...s.split(" ").map (s => s.length));
+}
+
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
