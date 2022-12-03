1e889aa2c8a6cfb4176e9c1440b3b7b7028ab46e
MacheteBy <Macheteby@gmail.com>
2022-12-03 10:30:23 +0300

    first

--- script.js
+++ script.js
@@ -406,7 +406,7 @@ function checkForFactor(numbers) {
 function stray(numbers) {
     for (var i in numbers) {
         if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
-            return numbers[i]
+            return numbers[i];
         }
     }
 }
