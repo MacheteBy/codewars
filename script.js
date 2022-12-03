c985aed36bed2b07295a0b7977e5afd52fb82250
MacheteBy <Macheteby@gmail.com>
2022-12-03 11:15:37 +0300

    first

--- script.js
+++ script.js
@@ -452,8 +452,10 @@ function likes(names) {
         return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     }else if(col > 3){
         return `${names[0]}, ${names[1]} and ${names.length-2} others like this`;
+    }else if(col == 0){
+        return  `no one likes this`;
     }
     return col;
   }
 
-  console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
+  console.log(likes([]));
