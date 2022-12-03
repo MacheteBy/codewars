e61d94c8d5d9cc3656dee3e8f1895f24181a24f3
MacheteBy <Macheteby@gmail.com>
2022-12-03 12:47:42 +0300

    first

--- script.js
+++ script.js
@@ -518,7 +518,8 @@ console.log(SeriesSum(0));
 //next
 
 function sumTwoSmallestNumbers(numbers) {  
-    //Code here
+    let sort = numbers.sort((a, b) => a -b);
+    return sort[0] + sort[1];
   }
 
-console.log(sumTwoSmallestNumbers());
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
