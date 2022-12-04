b200d2553f85a8cfda312aa989b473df84cbcf96
MacheteBy <Macheteby@gmail.com>
2022-12-04 10:52:27 +0300

    first

--- script.js
+++ script.js
@@ -570,7 +570,18 @@ function findOdd(A) {
 }
 
 function findOdd(A) {
-    
+    for(let i = 0; i < A.length; i++){
+        let col = 0;
+        for(let j = 0; j < A.length; j++){
+            if(A[i] == A[j]){
+                col++;
+            }
+            if(col % 2 !== 0){
+                return A[i];
+            }
+        }
+        return col;
+    }
 }
 
-console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
