d34c57aae52c5c1bceb429377111cb21ae0b29e4
MacheteBy <Macheteby@gmail.com>
2022-12-06 00:09:35 +0300

    cat years2

--- script.js
+++ script.js
@@ -703,10 +703,21 @@ console.log(litres(1.4));
 //next
 
 var humanYearsCatYearsDogYears = function (humanYears) {
-    // Your code here!
-    return [0, 0, 0];
+    let catYears = 0;
+    let dogYears = 0;
+    if(humanYears == 1){
+        catYears = 15;
+        dogYears = 15;
+    }else if(humanYears == 2){
+        catYears = 15+9;
+        dogYears = 15+9;
+    }else if(humanYears >= 3){
+        catYears = 24+((humanYears-2) * 5);
+        dogYears = 24+((humanYears-2) * 4);
+    }
+    return [humanYears, dogYears, catYears];
 };
 
 
 
-console.log(humanYearsCatYearsDogYears(1));
+console.log(humanYearsCatYearsDogYears(10));
