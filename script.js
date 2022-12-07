a7d2aab41edca956b487cdb55d53d14244ab6baf
MacheteBy <Macheteby@gmail.com>
2022-12-07 10:43:56 +0300

    grade full

--- script.js
+++ script.js
@@ -765,7 +765,14 @@ console.log(grow([1, 2, 3]));
 
 //next
 function finalGrade (exam, projects) {
-    return // final grade
-  }
+    if (exam > 90 || projects > 10) {
+        return 100;
+      } else if (exam > 75 && projects >= 5) {
+        return 90;
+      } else if (exam > 50 && projects >= 2) {
+        return 75;
+      }
+      return 0;
+    }
 
-  console.log(finalGrade(100, 12));
+  console.log(finalGrade(0, 3));
