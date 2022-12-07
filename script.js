27459b82335f28fa6c9649d8903f12ebb009fee0
MacheteBy <Macheteby@gmail.com>
2022-12-07 11:08:50 +0300

    frade book test

--- script.js
+++ script.js
@@ -811,7 +811,19 @@ console.log(persistence(39));
 //next
 
 function getGrade (s1, s2, s3) {
-    // Code here
+    let score = Math.trunc((s1 + s2 + s3) / 3);
+    console.log(score);
+    if(90 <= score <= 100){
+        return 'A';
+    }else if(80 <= score < 90){
+        return 'B';
+    }else if(70 <= score < 80){
+        return 'C';
+    }else if(60 <= score < 70){
+        return 'D';
+    }else if(0 <= score < 60){
+        return 'F';
+    }
   }
 
-  console.log(getGrade(95,90,93));
+  console.log(getGrade(84,79,85));
