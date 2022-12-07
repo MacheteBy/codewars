a213363dae3461c6e91f6474516417479f97ea14
MacheteBy <Macheteby@gmail.com>
2022-12-07 10:49:01 +0300

    number full

--- script.js
+++ script.js
@@ -781,7 +781,15 @@ console.log(finalGrade(0, 3));
 //next
 
 function getDivisorsCnt(n){
-    // todo
+    let count = 0;
+    for(let i = 0; i <= n; i++){
+        if(n%i == 0){
+            count++;
+        }
+    }
+    return count;
 }
 
 console.log(getDivisorsCnt(10));
+
+console.log(10%9);
