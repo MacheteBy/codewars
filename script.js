32043a14025b1b03cf9cef48d944592727257271
MacheteBy <Macheteby@gmail.com>
2022-12-08 13:13:47 +0300

    return negative

--- script.js
+++ script.js
@@ -1062,3 +1062,11 @@ function points(games) {
 }
 
 console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]));
+
+
+//next
+function makeNegative(num) {
+    return -num;
+  }
+
+console.log(makeNegative(42));
