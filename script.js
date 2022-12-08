e7637701e850e7786c7cfbbf377aa6a0e61cff75
MacheteBy <Macheteby@gmail.com>
2022-12-08 12:05:43 +0300

    calc

--- script.js
+++ script.js
@@ -942,3 +942,21 @@ var countSheep = function (num){
 };
 
 console.log(countSheep(3));
+
+//next
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
+
