e87007f706771b44bf16bc8971f85f86a5a46aa4
MacheteBy <Macheteby@gmail.com>
2022-12-14 11:08:40 +0300

    next cata v2

--- script.js
+++ script.js
@@ -1401,7 +1401,7 @@ console.log(towerBuilder(2));
 
 //next
 function index(array, n) {
-    
+    return array.length >= n ? Math.pow(array[n], n) : -1;
 }
 
-console.log(index([1, 2, 3, 4],2));
+console.log(index([1, 3, 10, 100],3));
