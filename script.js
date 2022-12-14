5d07e4f023d354c120fcca2732e12207c62a63ee
MacheteBy <Macheteby@gmail.com>
2022-12-14 11:11:03 +0300

    next cata

--- script.js
+++ script.js
@@ -1404,4 +1404,7 @@ function index(array, n) {
     return array[n] == undefined ? -1 : Math.pow(array.find((element) => element == array[n]), n);
 }
 
-console.log(index([1, 3, 10, 100],3));
+console.log(index([1, 3, 10, 100], 3));
+
+
+//next
