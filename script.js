3c5eb447e22a8b1c617de2a0550246ee88accdea
MacheteBy <Macheteby@gmail.com>
2022-12-15 11:11:34 +0300

    sum v1

--- script.js
+++ script.js
@@ -1440,3 +1440,13 @@ console.log(mergeArrays());
 
 
 //next
+function sumDigits(number) {
+    let arr = Math.abs(number).toString().split('')
+    let sum = 0;
+    
+    for(let i = 0; i < arr.length; i++)
+      sum += +arr[i]
+    return sum;
+  }
+
+  
