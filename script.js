bb4b183fe11d510cfd7eb797e61b22e66ba861cf
MacheteBy <Macheteby@gmail.com>
2022-12-15 11:13:38 +0300

    fact v1

--- script.js
+++ script.js
@@ -1454,7 +1454,7 @@ console.log(sumDigits(99));
 function factorial(n) {
     let result = 1;
     for (let i = 2; i <= n; i++) {
-      result *= i;
+        result *= i;
     }
     return result;
-  }
+}
