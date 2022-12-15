fc6c5f8d874170ddf1b0071fb33a626f2442f474
MacheteBy <Macheteby@gmail.com>
2022-12-15 11:08:40 +0300

    size v1

--- script.js
+++ script.js
@@ -1425,6 +1425,13 @@ console.log(hello('johN'));
 
 
 //next
-function mouthSize(animal) {
-    // code here
+function mergeArrays(arr1, arr2) {
+    let arr = arr1.concat(arr2).sort((a, b) => a - b);
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== arr[i + 1]) {
+        result.push(arr[i]);
+      }
+    }
+    return result;
   }
