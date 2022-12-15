f6b6c7267dea23e050d608d5bf2d57ca4474097e
MacheteBy <Macheteby@gmail.com>
2022-12-15 11:11:06 +0300

    туче

--- script.js
+++ script.js
@@ -1413,9 +1413,9 @@ function hello(name) {
     let hi = "Hello, ";
     let world = "World";
     let sign = "!";
-    if(name.length > 0){
+    if (name.length > 0) {
         return hi + name[0].toUpperCase() + name.slice(1).toLowerCase() + sign;
-    }else{
+    } else {
         return hi + world + sign;
     }
     // return name.length > 0 ? "Hello, " + name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase() + "!" : "Hello, World!";
@@ -1429,11 +1429,14 @@ function mergeArrays(arr1, arr2) {
     let arr = arr1.concat(arr2).sort((a, b) => a - b);
     let result = [];
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== arr[i + 1]) {
-        result.push(arr[i]);
-      }
+        if (arr[i] !== arr[i + 1]) {
+            result.push(arr[i]);
+        }
     }
     return result;
-  }
+}
 
-  console.log(mergeArrays());
+console.log(mergeArrays());
+
+
+//next
