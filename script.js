90b1f13d9f4127e086f9906335ea261df610addf
MacheteBy <Macheteby@gmail.com>
2022-12-19 10:38:18 +0300

    next cata v6

--- script.js
+++ script.js
@@ -1565,4 +1565,4 @@ function simpleMultiplication(number) {
     return number % 2 == 0 ? number * 8 : number * 9;
 }
 
-console.log(simpleMultiplication(2));
+console.log(simpleMultiplication(1));
