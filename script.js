e232bb262cd1807249f6d2622d9b1bc6dd752ea6
MacheteBy <Macheteby@gmail.com>
2022-12-20 11:08:35 +0300

    next cata v3

--- script.js
+++ script.js
@@ -1644,3 +1644,16 @@ function alternativeCase(string){
     }
   return newString.join('');
 }
+
+function alternativeCase(string){
+    var output = "";
+    for(var i = 0; i < string.length; i++){
+        if (i % 2 != 0) {
+            output += string[i].toUpperCase();
+        }
+        else {
+            output += string[i].toLowerCase();
+         }   
+    }
+    return output;
+}
