e689d7a8f8f84f26366ceedc9b8251276e7d41ca
MacheteBy <Macheteby@gmail.com>
2022-12-21 00:19:32 +0300

    multi v4

--- script.js
+++ script.js
@@ -1639,10 +1639,13 @@ function multiTable(number) {
     for(let i = 1; i <= 10; i++){
         res += `${i} * ${number} = ${i * number}\n`;
     }
-    return res;
+    return res.split(0, 2);
 }
 
 console.log(multiTable(5));
 
 
 console.log('1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50');
+
+'1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50\n'
+'1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
