a7ff3c1a81f52dea7e2c1ca0e54bb9c3d7ce5f1b
MacheteBy <Macheteby@gmail.com>
2022-12-21 00:14:29 +0300

    multi v3

--- script.js
+++ script.js
@@ -1635,7 +1635,14 @@ console.log(main('take ', 'item'));
 
 //next
 function multiTable(number) {
-    // good luck
+    let res = "";
+    for(let i = 1; i <= 10; i++){
+        res += `${i} * ${number} = ${i * number}\n`;
+    }
+    return res;
 }
 
 console.log(multiTable(5));
+
+
+console.log('1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50');
