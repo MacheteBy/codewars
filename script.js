e39d1222578c99b5db145d9ca1bde86e421d4bfc
MacheteBy <Macheteby@gmail.com>
2022-12-25 11:10:35 +0300

    arithmetic full

--- script.js
+++ script.js
@@ -1765,8 +1765,19 @@ console.log(replace("!Hi! Hi!"));
 
 //next
 function arithmetic(a, b, operator) {
-    //your code here!
+    if(operator == 'add'){
+        return a + b;
+    }else if(operator == 'subtract'){
+        return a - b;
+    }else if(operator == 'multiply'){
+        return a * b;
+    }else if(operator == 'divide'){
+        return a / b;
+    }
 }
 
 
 console.log(arithmetic(1, 2, "add"));
+
+
+//next
