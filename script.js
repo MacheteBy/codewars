2c470a6d3986daf02ac323da2a8167c4e9508d9b
MacheteBy <Macheteby@gmail.com>
2023-11-30 23:07:00 +0300

    30,11,23

--- script.js
+++ script.js
@@ -2045,3 +2045,25 @@ function largestPairSum(numbers) {
 
 
 console.log(largestPairSum([10,14,2,23,19]))
+
+
+function longest(s1, s2) {
+    let strFull = s1 + s2;
+    let outStr = [...new Set(strFull)].sort().join('');
+    return outStr;
+}
+
+
+console.log(longest("aretheyhere", "yestheyarehere"))
+
+
+function rainAmount(mm){
+    if (mm < 40) {
+         return `You need to give your plant ${40 - mm}mm of water`
+    }
+    else {
+         return 'Your plant has had more than enough water for today!'
+    };
+}
+
+console.log(rainAmount(39))
